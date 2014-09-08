7ca210e4bea3b3738c0a8cc59530df459b2bc4af
koopero <koopero@gmail.com>
2014-09-08 03:32:00 -0700

    Scour out Source

--- js/Source.js
+++ js/Source.js
@@ -14,7 +14,6 @@ const REGEX = {
 	VAR_DEC: /\s*([a-zA-Z_][a-zA-Z0-9]+)\s*((\[(.*)\])*)$/,
 	COMMENTS_MULTILINE: /\/\*.*?\*\//mg,
 	COMMENTS_LINE: /\/\/.*?$/mg,
-	ENUM_DEC: /(\w+)\s*=\s*(\d+)/g,
 	isStruct: /struct\s+([a-zA-Z_][a-zA-Z0-9]+)\s*\{/
 
 }
@@ -51,16 +50,6 @@ function isolateSection ( source, section ) {
 	return match ? match[1] : '';
 }
 
-exports.getEnums = getEnums;
-function getEnums( source ) {
-	var ret = {};
-	source.replace( REGEX.ENUM_DEC, function ( match, name, num ) {
-		ret[name] = parseInt( num );
-	});
-
-	return ret;
-}
-
 
 function parseSource ( source ) {
 	var parsed = {
@@ -91,32 +80,6 @@ function parseSource ( source ) {
 
 
 
-function parseVarsDeclaration ( source, parsed ) {
-	var offset = 0;
-	var vars = {};
-
-	source = removeComments( source );
-
-
-	var match;
-	while ( match = REGEX.VAR_LINE.exec( source ) ) {
-		var line = match[1];
-		var _var = parseVar( line, parsed );
-		if ( _var ) {
-			_var.comment = match[3];
-			_var.offset = offset;
-			
-
-			_var = new Variable( _var.name, _var );
-			offset += _var.size;
-
-			vars[ _var.name] = _var;			
-		}
-	}
-
-	return vars;
-}
-
 function removeComments ( source ) {
 	source = source.replace( REGEX.COMMENTS_MULTILINE, '' );
 	//source = source.replace( REGEX.COMMENTS_LINE, '' );
@@ -124,52 +87,6 @@ function removeComments ( source ) {
 	return source;	
 }
 
-function parseVar ( line, parsed ) {
-	line = _s.trim( line );
-	if ( !line || !line.length )
-		return;
-
-	var v = {};
-
-	for ( var typeName in Types ) {
-		var rest = beginsWith( line, typeName );
-		if ( rest ) {
-			v.type = Types[typeName];
-			break;
-		}
-	}
-	if ( !v.type )
-		throw ParseError( "unknown type", line );
-
-	var match = REGEX.VAR_DEC.exec( rest );
-	if ( !match ) {
-		throw ParseError( "can't understand declaration", line );
-	}
-
-	v.name = match[1];
-	v.length = 1;
-	v.dims = [];
-	var dim = match[4];
-
-	if ( dim ) {
-		dim = dim.replace( /\s/, '' );
-
-		var dims = dim.split( '][' );
-		dims.forEach( function ( dim ) {
-
-			if ( parsed.define[dim] )
-				dim = parsed.define[dim];
-
-			dim = parseInt( dim );
-			if ( isNaN( dim ) )
-				throw ParseError( "can't understand array dimension", dim );
-
-			v.dims.push( dim );
-		});
-	}
-
-	return v;
-}
 
 function ParseError ( error, subject ) {
 	return new Error ( error + " "+subject );
